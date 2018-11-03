33e3d2ea9c9e35f091ca075e09f92d54aa8c6924
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-11-03 13:35:02 +0200

    add counter

---
+++ hello-world/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    ttt
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <MyTest />
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyTest from './components/MyTest.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    MyTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/MyTest.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="MyTest">
+    <button v-on:click="increment">increment</button>
+    this is  test
+    <p>Counter: {{ counter }}</p>
+    <p>Clicks: {{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTest',
+  data: () =>( {
+    counter: 0,
+    clicks: 0
+  }),
+  methods: {
+    increment (e) {
+      console.log('el',e);
+     // alert('test');
+      this.counter  = this.counter + 2;
+      this.clicks++;
+    }
+  } 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
